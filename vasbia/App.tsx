94532ef738eb4eaae2f6dbfd4e160a75f0deca31
name <joyyer02@gmail.com>
2025-09-11 01:41:40 +0700

    Change center to Engineering building

--- vasbia/App.tsx
+++ vasbia/App.tsx
@@ -6,9 +6,9 @@ export default function App() {
   return (
     <View style={styles.page}>
       <MapView style={styles.map} mapStyle="https://maptiler.code4.dad/api/maps/bangkok/style.json">
-        <Camera centerCoordinate={[100.5018, 13.7563]} zoomLevel={12}/>
+        <Camera centerCoordinate={[100.772451, 13.727075]} zoomLevel={18}/>
         {/* <PointAnnotation id={"test"} coordinate={[100.5018, 13.7563]}/> */}
-        <MarkerView coordinate={[100.5018, 13.7563]}>
+        <MarkerView coordinate={[100.772451, 13.727075]}>
           <View style={styles.marker} />
         </MarkerView>
       </MapView>
