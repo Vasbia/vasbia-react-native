e20d7b4eed206537227773296384791dabbc6d2e
name <joyyer02@gmail.com>
2025-09-24 20:55:09 +0700

    Update MapScreen.tsx

--- vasbia/src/screens/MapScreen.tsx
+++ vasbia/src/screens/MapScreen.tsx
@@ -1,28 +1,65 @@
-import { View, StyleSheet} from 'react-native';
-import { MapView, Camera, MarkerView} from '@maplibre/maplibre-react-native';
+import { useState, useRef } from "react";
+import { View, StyleSheet } from 'react-native';
+import { MapView, Camera, MarkerView, CameraRef } from '@maplibre/maplibre-react-native';
+import { useFlyTo } from "../map/useFlyTo";
 
-import FloatingIconButton from "../components/FloatingIconButton";
-import ToggleModeBIcon from "../assets/icons/ToggleModeBIcon";
-import RatingBIcon from "../assets/icons/RatingBIcon";
-import NotificationBIcon from "../assets/icons/NotificationBIcon";
-import RouteIcon from '../assets/icons/RouteIcon';
-import LandmarkIcon from '../assets/icons/LandmarkIcon';
+import ToggleModeButton from "../components/ToggleModeButton";
+import RatingButton from "../components/RatingButton";
+import NotificationButton from "../components/NotificationButton";
+import RenderAllBusStops from "../map/RenderBusStop";
+import RenderAllBusRoutes from "../map/RenderBusRoute";
+import RenderAllLandmarks from "../map/RenderLandmark";
+
+type MapMode = "bus" | "landmark";
 
 export default function MapScreen() {
+  const [initialSet, setInitialSet] = useState(false);
+  const [mode, setMode] = useState<MapMode>("bus");
+  const [selectedId, setSelectedId] = useState<string | null>("null");
+  
+  const cameraRef = useRef<CameraRef>(null);
+  const flyTo = useFlyTo(cameraRef);
 
   return (
     <View style={styles.page}>
-      <MapView style={styles.map} mapStyle="https://maptiler.code4.dad/api/maps/bangkok/style.json">
-        <Camera centerCoordinate={[100.772451, 13.727075]} zoomLevel={18}/>
+      <MapView style={styles.map} mapStyle="https://maptiler.code4.dad/api/maps/bangkok/style.json"
+        onDidFinishLoadingMap={() => {
+          if (!initialSet) {
+            cameraRef.current?.setCamera({
+              centerCoordinate: [100.772451, 13.727075],
+              zoomLevel: 18,
+              animationDuration: 1000,
+            });
+            setInitialSet(true);
+          }
+        }}
+      >
+        <Camera ref={cameraRef} />
+        
         <MarkerView coordinate={[100.772451, 13.727075]}>
           <View style={styles.marker} />
         </MarkerView>
+
+        {mode === "bus" && (
+          <>
+            <RenderAllBusRoutes selectedId={selectedId} setSelectedId={setSelectedId} />
+            <RenderAllBusStops selectedId={selectedId} setSelectedId={setSelectedId} flyTo={flyTo}/>
+          </>
+        )}
+
+        {mode === "landmark" && (
+          <RenderAllLandmarks selectedId={selectedId} setSelectedId={setSelectedId} flyTo={flyTo}/>
+        )}
+
       </MapView>
 
       <View style={styles.buttonContainer}>
-        <ToggleModeBIcon iconOn={<RouteIcon  />} iconOff={<LandmarkIcon  />}/>
-        <FloatingIconButton icon={<RatingBIcon />} />
-        <FloatingIconButton icon={<NotificationBIcon />} />
+        <ToggleModeButton onToggle={(isBusMode) => {
+          setMode(isBusMode ? "bus" : "landmark");
+          setSelectedId(null);
+        }} />
+        <RatingButton />
+        <NotificationButton />
       </View>
 
     </View>
@@ -47,5 +84,7 @@ const styles = StyleSheet.create({
     top: '35%',
     justifyContent: 'center',
     alignItems: 'center',
+    gap: 7,
   },
+
 });
