56de1b64153cdfa627333ab16728a598e54aa5f3
name <joyyer02@gmail.com>
2025-09-24 20:54:09 +0700

    Clickable components in map

---
+++ vasbia/src/components/BusStopButton.tsx
@@ -0,0 +1,30 @@
+import { TouchableOpacity, StyleSheet } from "react-native";
+import BusStopIcon from "../assets/icons/BusStopIcon";
+
+type BusStopButtonProps = {
+  selected?: boolean;
+  onPress?: () => void;
+};
+
+export default function BusStopButton({ selected, onPress }: BusStopButtonProps) {
+    const size = selected ? 45 : 30; 
+  return (
+    <TouchableOpacity 
+      style={[
+        styles.container,
+        { width: size, height: (size * 41) / 29 },
+      ]}
+      onPress={onPress}
+      activeOpacity={0.7}
+    >
+      <BusStopIcon size={size} color={selected ? "#FF0000" : "#2D6EFF"} />
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
